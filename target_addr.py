af5d34110b4b4f6e245fc6cc2acd25c6ab73e7ed
Ioannis Kokkinis <info@upg.gr>
2019-10-16 16:20:45 +0300

    Updated target_addr.py.

--- target_addr.py
+++ target_addr.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #target address to spider
 #def_addr = "http://sites.test/test.m3u"
-#def_addr = "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u"
-def_addr = "helper/android.m3u" 
+def_addr = "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u"
+#def_addr = "helper/android.m3u"
 #us-east-1
 #ca-central-1
 #eu-central-1
