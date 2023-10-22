fb72197db293039e4c8d9ae9ba3925979692b875
Ioannis Kokkinis <info@upg.gr>
2023-10-22 22:45:04 +0300

    f
    f

--- target_addr.py
+++ target_addr.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 #target address to spider
 #def_addr = "http://sites.test/test.m3u"
-def_addr = "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u"
+#def_addr = "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/android.m3u"
+def_addr = "https://raw.githubusercontent.com/lentisgr/greek-iptv/master/android.m3u"
 def_addr_2 = "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/radio.m3u"
 #def_addr = "helper/android.m3u"
 #us-east-1
